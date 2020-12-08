df6bf494a87c63fb7d6db1b8d91fc10edda205d7
Kira <priyanshushrama709@gmail.com>
2020-12-08 10:12:48 +0530

    style gallery

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Navbar from "./components/Navbar";
-import Form from "./components/Form"
+import Form from "./components/Form";
+import Gallery from "./components/movieGallery";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Form />
+      <Gallery />
     </div>
   );
 }
---
+++ src/components/movieGallery.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import "../styles/gallery.css";
+
+export default function Gallery() {
+    return (
+        <section className="gallery">
+            case
+        </section>
+    )
+}
